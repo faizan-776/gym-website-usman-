63175e27d0778fe7d8618728dfc62c592ad1888b
Usman Siddiqui <siddiquishoaib515@gmail.com>
2025-03-21 09:43:29 +0500

    working on 3rd container of home

--- client/src/pages/home/containers/HomeThird.jsx
+++ client/src/pages/home/containers/HomeThird.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import strength from "../../../assets/pexels-victorfreitas-791763 (1).webp";
+import cardio from "../../../assets/pexels-olly-3763868.webp";
+import yoga from "../../../assets/pexels-olly-868757.webp";
+import weight from "../../../assets/young-fitness-woman-measure-with-tape-her-belly-isolated-white-wall.webp";
+import martial from "../../../assets/karate-round-kick-punching-bag.webp";
+import athletic from "../../../assets/front-view-fit-man-running-equipment.webp";
 
 const Card = ({ img, title, description }) => {
   return (
     <div className="home__homethird-cards_card">
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <div className="home__homethird-cards_card-img">
+        <img src={img} alt={title} />
+      </div>
+      <div className="home__homethird-cards_card-detail">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
@@ -13,37 +23,37 @@ const Card = ({ img, title, description }) => {
 const HomeThird = () => {
   const trainingPrograms = [
     {
-      img: "path-to-image-1",
+      img: strength,
       title: "Strength Training",
       description:
         "Build muscle and improve endurance with guided strength training.",
     },
     {
-      img: "path-to-image-2",
+      img: cardio,
       title: "Cardio Workouts",
       description:
         "Boost your heart health with high-intensity cardio exercises.",
     },
     {
-      img: "path-to-image-3",
+      img: yoga,
       title: "Yoga & Flexibility",
       description:
         "Improve flexibility and mental clarity with structured yoga programs.",
     },
     {
-      img: "path-to-image-4",
+      img: weight,
       title: "Weight Loss",
       description:
         "Burn calories effectively with personalized weight loss training.",
     },
     {
-      img: "path-to-image-5",
+      img: martial,
       title: "Martial Arts",
       description:
         "Learn self-defense and enhance discipline with martial arts training.",
     },
     {
-      img: "path-to-image-6",
+      img: athletic,
       title: "Athletic Performance",
       description:
         "Train like an athlete with high-performance sports conditioning.",
