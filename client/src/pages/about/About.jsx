ab3605500dcb59e9bc464ef12edbcc4efebc7fba
Usman Siddiqui <siddiquishoaib515@gmail.com>
2025-03-30 01:19:12 +0500

    about page completed

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./about.css";
 import AboutFirst from "./containers/AboutFirst";
 import AboutSecond from "./containers/AboutSecond";
+import AboutThird from "./containers/AboutThird";
 
 const About = () => {
   return (
     <div className="about">
       <AboutFirst />
       <AboutSecond />
+      <AboutThird />
     </div>
   );
 };
--- client/src/pages/about/containers/AboutThird.jsx
+++ client/src/pages/about/containers/AboutThird.jsx
@@ -1,12 +1,8 @@
 import React from "react";
+import PhotoGallery from "../../../components/carousel/Carousel"
 import trainer1 from "../../../assets/trainers/gymtrainer1.webp";
 import trainer2 from "../../../assets/trainers/gymtrainer2.webp";
 import trainer3 from "../../../assets/trainers/gymtrainer3.webp";
-import trainer4 from "../../../assets/trainers/gymtrainer4.webp";
-import trainer5 from "../../../assets/trainers/gymtrainer5.webp";
-import trainer6 from "../../../assets/trainers/gymtrainer6.webp";
-
-import Photogallery from "../../../components/carousel/Carousel";
 
 import img1 from "../../../assets/carousel/brand-1.webp";
 import img2 from "../../../assets/carousel/brand-2.webp";
@@ -14,45 +10,40 @@ import img3 from "../../../assets/carousel/brand-3.webp";
 import img4 from "../../../assets/carousel/brand-4.webp";
 import img5 from "../../../assets/carousel/brand-5.webp";
 
-const TrainersSecond = () => {
+const AboutThird = () => {
   const trainers = [
     { name: "Usman Siddiqui", image: trainer1 },
     { name: "Faizan Siddiqui", image: trainer2 },
     { name: "Eshal Zahra", image: trainer3 },
-    { name: "Muhib Ahmed", image: trainer4 },
-    { name: "M. Hassan", image: trainer5 },
-    { name: "Sabeh Khan", image: trainer6 },
   ];
   const images = [img1, img2, img3, img4, img5];
-
   return (
-    <div className="trainers__trainerssecond">
+    <div className="about__aboutthird">
       <div className="container">
-        <div className="trainers__trainerssecond-heading">
+        <div className="home__homefourth-heading">
           <h2>Meet Our Team</h2>
-          <h1>Expert Trainers</h1>
+          <h1>Meet Our Expert Trainers</h1>
         </div>
-        <div className="trainers__trainerssecond-experts">
+        <div className="home__homefourth-experts">
           {trainers.map((trainer, index) => (
-            <div key={index} className="trainers__trainerssecond-experts_card">
+            <div key={index} className="home__homefourth-experts_card">
               <img
                 src={trainer.image}
                 alt={trainer.name}
-                className="trainers__trainerssecond-experts_card-img"
+                className="home__homefourth-experts_card-img"
               />
-              <div className="trainers__trainerssecond-experts_overlay">
+              <div className="home__homefourth-experts_overlay">
                 <h3>{trainer.name}</h3>
               </div>
             </div>
           ))}
         </div>
-        <hr />
-        <div className="trainers__trainerssecond-gallery">
-          <Photogallery images={images} />
+        <div className="home__homefourth-carousal">
+          <PhotoGallery images={images} />
         </div>
       </div>
     </div>
   );
 };
 
-export default TrainersSecond;
+export default AboutThird;
