5766ca34bde689a933dab63226cd4eab032c1c6a
Usman Siddiqui <siddiquishoaib515@gmail.com>
2026-02-28 16:17:50 +0500

    gsap installed

--- client/src/pages/home/containers/HomeFirst.jsx
+++ client/src/pages/home/containers/HomeFirst.jsx
@@ -24,7 +24,7 @@ const Homefirst = () => {
             toggleActions: "play none none none",
           },
           immediateRender: false,
-        }
+        },
       );
     }, contentRef);
 
